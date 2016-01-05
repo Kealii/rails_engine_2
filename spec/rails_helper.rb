fc243006a90bfe3674a34498bcb1d46bc1d88c16
Michael Wong <michael.brian.wong@gmail.com>
2016-01-05 14:40:39 -0700

    Limits JSON default to Controller Tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,7 +13,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
-  config.before :each do
+  config.before :each, type: :controller do
     request.headers["accept"] = 'application/json'
   end
 end
